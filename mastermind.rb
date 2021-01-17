3a081cec3a2ca0cd6990f2e633e30c09d1883301
Nicholas <nicholasbarizo@gmail.com>
2021-01-16 20:30:55 -0800

    added function that obtains four guessed colors from the user

--- mastermind.rb
+++ mastermind.rb
@@ -9,7 +9,10 @@ class Mastermind
 
   def play
     code = create_code
-    code.each { |word| puts word }
+    
+    puts 'Choose four colors from the following: Red, Orange, Yellow, Green, Blue, Purple'
+    guess = get_guess
+    puts guess
   end
 
   def create_code
@@ -19,6 +22,35 @@ class Mastermind
       code.push colors.sample
     end
     code
+
+  end
+
+  def get_guess
+    input = gets.chomp.downcase.gsub(/,/, '').split
+    possible_inputs = %w[red r orange o yellow y green g blue b purple p]
+    allowed_input = []
+    input.each do |color|
+      allowed_input.push(color) if possible_inputs.include?(color)
+    end
+    while allowed_input.length < 4
+      color_plural = allowed_input.length == 3 ? '' : 's'
+      puts "You need to pick #{4 - allowed_input.length} more color#{color_plural}"
+      input = gets.chomp.gsub(/,/, '').split
+      input.each do |color|
+        allowed_input.push(color) if possible_inputs.include?(color)
+      end
+    end
+    allowed_input.pop while allowed_input.length > 4
+    allowed_input.map do |color|
+      case color
+      when 'r' then 'red'
+      when 'o' then 'orange'
+      when 'y' then 'yellow'
+      when 'g' then 'green'
+      when 'b' then 'blue'
+      when 'p' then 'purple'
+      end
+    end
   end
 end
 
